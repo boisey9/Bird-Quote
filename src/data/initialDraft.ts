2d6da2580d222938e880ff86887bc091d3bc075f
boisey9 <erikboisvert9@gmail.com>
2026-06-05 15:28:20 -0400

    Add RFQ initial draft data

---
+++ src/data/initialDraft.ts
@@ -0,0 +1,28 @@
+import { featureCategories } from './rfqData';
+import type { RfqDraft } from '../types/rfq';
+
+export const initialDraft: RfqDraft = {
+  company: {
+    dealerName: 'A. GIRARDIN INC.',
+    dealerContact: 'ERIK BOISVERT',
+    finalCustomerName: 'Westview Resort & Casino',
+    finalCustomerPhone: '(819) 555-1234',
+    provinceState: 'CANADA',
+    additionalInfo: 'Delivery to main resort entrance.',
+    referenceMode: 'pastOrder',
+    pastQuoteOrOrderNumber: 'MB-2024-08215'
+  },
+  specs: {
+    chassis: 'ford',
+    wheelbase: '158-drw',
+    busType: 'hotel',
+    quantity: 1,
+    seatingCapacity: 16,
+    wheelchairCapacity: 1
+  },
+  features: featureCategories.flatMap((category) =>
+    category.options.slice(0, category.id === 'seats' ? 4 : 2)
+  ),
+  confirmedAccuracy: true,
+  consentToContact: true
+};
