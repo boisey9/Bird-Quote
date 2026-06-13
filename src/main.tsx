8efe6ec687b36182605af4fe3fb09698040b84e0
boisey9 <erikboisvert9@gmail.com>
2026-06-13 14:45:44 -0400

    Import shared production UI styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
+import './styles/uiTokens.css';
+import './components/common/commonUi.css';
 import './components/Header.css';
 import './components/ui/ButtonSystem.css';
 import './components/pages/AdminCms.css';
